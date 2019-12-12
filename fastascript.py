d984b46929fd0872e34eae3e4d7d640305d4294c
Ayesha <ayesha.s2022@curtishs.org>
2019-12-12 18:02:40 -0500

    a function that creates a fasta file for a sequence

---
+++ fastascript.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3 
+import argparse
+import FileIO
+import Random
+import OS
+import numpy as np
+import random
+#importing libraries
+#parser = argparse.ArgumentParser(description = "insert header")
+#parser.add_argument("-header",required)
+#args = pareser.parse_args()
+#this function makes a repeat sequence
+
+nuc_list = ["A","T","C",G"]
+
+def repeat_seq(length):
+    your_letters='ATCG'
+    return ''.join((random.choice(your_letters) for i in range(length)))
+
+ 
+#new_fasta is the name of our fasta function
+repeat_seq1= repeat_seq(30)
+repeat_seq2= repeat_seq(30)
+repeat_seq3= repeat_seq(30)
+repeat_seq4= repeat_seq(30)
+repeat_seq5= repeat_seq(30)
+
+def new_fasta():
+	try:
+		with open("psuedo_protein.fasta",w) as in_handle:
+			in_handle.write(repeat_seq1,repeat_seq2,repeat_seq3,repeat_seq4,repeat_seq5)
+
+
+#	except as IOError:
+	#this is the exception error that will help the code
+#		print("IOError")
+
